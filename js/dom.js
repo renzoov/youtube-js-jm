ceba00a64195e225d5a1a4b3e62735b6c864ed58
Renzo Osorio <renzo.osorio1909@gmail.com>
2022-11-12 01:30:42 -0500

    video 73: DOM: Eventos con Parámetros y Remover Eventos

--- js/dom.js
+++ js/dom.js
@@ -146,23 +146,262 @@
 
 /************ DOM: Texto y HTML ****************/
 
-const $whatIsDOM = document.getElementById("que-es");
+// const $whatIsDOM = document.getElementById("que-es");
 
-let text = `
-  <p>
-    El Modelo de Objeto del Documento (<b><i>DOM - Document Object Model</i></b>)
-    es un API para documentos HTML y XML.
-  </p>
-  <p>
-    Éste proveé una representación estructural del documento, permitiendo modificar su contenido
-    y presentación visual mediante código JS.
-  </p>
-  <p>
-    <mark>El DOM no es parte de la especificación de JavaScript, es una API para los navegadores</mark>
-  </p>
-`;
+// let text = `
+//   <p>
+//     El Modelo de Objeto del Documento (<b><i>DOM - Document Object Model</i></b>)
+//     es un API para documentos HTML y XML.
+//   </p>
+//   <p>
+//     Éste proveé una representación estructural del documento, permitiendo modificar su contenido
+//     y presentación visual mediante código JS.
+//   </p>
+//   <p>
+//     <mark>El DOM no es parte de la especificación de JavaScript, es una API para los navegadores</mark>
+//   </p>
+// `;
 
-$whatIsDOM.innerText = text;
-$whatIsDOM.textContent = text;
-$whatIsDOM.innerHTML = text;
-$whatIsDOM.outerHTML = text;
+// $whatIsDOM.innerText = text;
+// $whatIsDOM.textContent = text;
+// $whatIsDOM.innerHTML = text;
+// $whatIsDOM.outerHTML = text;
+
+/************ DOM: Traversing: Recorriendo el DOM  ****************/
+
+// const $cards = document.querySelector(".cards");
+// console.log($cards);
+// console.log($cards.childNodes);
+// console.log($cards.children);
+// console.log($cards.children[2]);
+// console.log($cards.parentElement);
+// console.log($cards.firstChild);
+// console.log($cards.firstElementChild);
+// console.log($cards.lastChild);
+// console.log($cards.lastElementChild);
+// console.log($cards.previousSibling);
+// console.log($cards.previousElementSibling);
+// console.log($cards.nextSibling);
+// console.log($cards.nextElementSibling);
+// console.log($cards.closest("div"));
+// console.log($cards.closest("body"));
+// console.log($cards.children[3].closest("section"));
+
+/************ DOM: Creando Elementos y Fragmentos ****************/
+
+// const $figure = document.createElement("figure");
+// const $img = document.createElement("img");
+// const $figcaption = document.createElement("figcaption");
+// const $figcaptionText = document.createTextNode("Animals");
+// const $cards = document.querySelector(".cards");
+// const $figure2 = document.createElement("figure");
+
+// $img.setAttribute("src", "https://placeimg.com/200/200/animals");
+// $img.setAttribute("alt", "Animals");
+// $figure.classList.add("card");
+
+// $figcaption.appendChild($figcaptionText);
+// $figure.appendChild($img);
+// $figure.appendChild($figcaption);
+// $cards.appendChild($figure);
+
+// $figure2.innerHTML = `
+//   <img src="https://placeimg.com/200/200/people" alt="People">
+//   <figcaption>People</figcaption>
+// `;
+
+// $figure2.classList.add("card");
+// $cards.appendChild($figure2);
+
+// const estaciones = ["Primavera", "Verano", "Otoño", "Invierno"];
+// const $ul = document.createElement("ul");
+
+// document.write("<h3>Estaciones del Año</h3>");
+// document.body.appendChild($ul);
+
+// estaciones.forEach((el) => {
+//   const $li = document.createElement("li");
+//   $li.textContent = el;
+//   $ul.appendChild($li);
+// });
+
+// const continentes = ["Africa", "América", "Asia", "Europa", "Oceanía"];
+// const $ul2 = document.createElement("ul");
+
+// document.write("<h3>Continentes del Mundo</h3>");
+// document.body.appendChild($ul2);
+// $ul2.innerHTML = "";
+// continentes.forEach((el) => ($ul2.innerHTML += `<li>${el}</li>`));
+
+// const meses = [
+//   "Enero",
+//   "Febrero",
+//   "Marzo",
+//   "Abril",
+//   "Mayo",
+//   "Junio",
+//   "Julio",
+//   "Agosto",
+//   "Septiembre",
+//   "Octubre",
+//   "Noviembre",
+//   "Diciembre",
+// ];
+
+// const $ul3 = document.createElement("ul");
+// const $fragment = document.createDocumentFragment();
+
+// meses.forEach((el) => {
+//   const $li = document.createElement("li");
+//   $li.textContent = el;
+//   $fragment.appendChild($li);
+// });
+
+// document.write("<h3>Meses del Año</h3>");
+// $ul3.appendChild($fragment);
+// document.body.appendChild($ul3);
+
+/************ DOM: Templates HTML ****************/
+
+// const $cards = document.querySelector(".cards"),
+//   $template = document.getElementById("template-card").content,
+//   $fragment = document.createDocumentFragment(),
+//   cardContent = [
+//     {
+//       title: "Tecnología",
+//       img: "https://placeimg.com/200/200/tech",
+//     },
+//     {
+//       title: "Animales",
+//       img: "https://placeimg.com/200/200/animals",
+//     },
+//     {
+//       title: "Arquitectura",
+//       img: "https://placeimg.com/200/200/arch",
+//     },
+//     {
+//       title: "Gente",
+//       img: "https://placeimg.com/200/200/people",
+//     },
+//     {
+//       title: "Naturaleza",
+//       img: "https://placeimg.com/200/200/nature",
+//     },
+//   ];
+
+// cardContent.forEach((el) => {
+//   $template.querySelector("img").setAttribute("src", el.img);
+//   $template.querySelector("img").setAttribute("alt", el.img);
+//   $template.querySelector("figcaption").textContent = el.title;
+
+//   let $clone = document.importNode($template, true);
+//   $fragment.appendChild($clone);
+// });
+
+// $cards.appendChild($fragment);
+
+/************ DOM: Modificando Elementos (Old Style) ****************/
+
+// const $cards = document.querySelector(".cards"),
+//   $newCard = document.createElement("figure"),
+//   $cloneCards = $cards.cloneNode(true);
+
+// $newCard.innerHTML = `
+//     <img src="https://placeimg.com/200/200/any" alt="Any">
+//     <figcaption>Any</figcaption>
+//   `;
+
+// $newCard.classList.add("card");
+// $cards.replaceChild($newCard, $cards.children[2]);
+// $cards.insertBefore($newCard, $cards.firstElementChild);
+// $cards.removeChild($cards.lastElementChild);
+
+// document.body.appendChild($cloneCards);
+
+/************ DOM: Modificando Elementos (Cool Style) ****************/
+
+/*
+ insertAdjacent...
+  insertAdjacentElement(position, el)
+  insertAdjacentHTML(position, html)
+  insertAdjacentText(position, text)
+
+ Posiciones:
+  beforebegin(hermano anterior)
+  afterbegin(primer hijo)
+  beforeend(ultimo hijo)
+  afterend(hermano siguiente)
+*/
+
+// const $cards = document.querySelector(".cards"),
+//   $newCard = document.createElement("figure"),
+//   $cloneCards = $cards.cloneNode(true);
+
+// const $contentCard = `
+//     <img src="https://placeimg.com/200/200/any" alt="Any">
+//     <figcaption></figcaption>
+//   `;
+
+// $newCard.classList.add("card");
+
+// $newCard.insertAdjacentHTML("beforeend", $contentCard);
+// $newCard.querySelector("figcaption").insertAdjacentText("afterbegin", "Any");
+
+// $cards.insertAdjacentElement("beforebegin", $newCard);
+// $cards.insertAdjacentElement("afterbegin", $newCard);
+// $cards.insertAdjacentElement("beforeend", $newCard);
+// $cards.insertAdjacentElement("afterend", $newCard);
+
+// $cards.prepend($newCard);
+// $cards.before($newCard);
+// $cards.append($newCard);
+// $cards.after($newCard);
+
+/************ DOM: Manejadores de Eventos ****************/
+
+// function holaMundo() {
+//   alert("Hola Mundo");
+//   console.log(event);
+// }
+
+// const $eventoSemantico = document.getElementById("evento-semantico"),
+//   $eventoMultiple = document.getElementById("evento-multiple");
+
+// $eventoSemantico.onclick = holaMundo;
+// $eventoSemantico.onclick = function (e) {
+//   alert("Hola Mundo Manejador de Eventos Semántico");
+//   console.log(e);
+// };
+
+// $eventoMultiple.addEventListener("click", holaMundo);
+// $eventoMultiple.addEventListener("click", (e) => {
+//   alert("Hola Mundo Manejador de Eventos Múltiple");
+//   console.log(e);
+//   console.log(e.type);
+//   console.log(e.target);
+// });
+
+/************ DOM: Eventos con Parámetros y Remover Eventos ****************/
+
+// function saludar(nombre = "Desconocido") {
+//   alert(`Hola ${nombre}`);
+// }
+
+// const $eventoMultiple = document.getElementById("evento-multiple");
+// const $eventoRemover = document.getElementById("evento-remover");
+
+// $eventoMultiple.addEventListener("click", () => {
+//   saludar();
+//   saludar("Pepe");
+// });
+
+// const removerDobleClick = (e) => {
+//   alert(`Removiendo el evento de tipo ${e.type}`);
+//   console.log(e);
+//   $eventoRemover.removeEventListener("dblclick", removerDobleClick);
+//   $eventoRemover.disabled = true;
+// };
+
+// $eventoRemover.addEventListener("dblclick", removerDobleClick);
+
+/************ DOM: Flujo de Eventos (Burbuja y Captura) ****************/
