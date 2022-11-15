96008e8d9e390b582b02d830ffd3606cdc45b0f7
Renzo Osorio <renzo.osorio1909@gmail.com>
2022-11-15 00:40:57 -0500

    video 80 DOM Ejercicios Prácticos Presentación

--- js/dom.js
+++ js/dom.js
@@ -501,3 +501,61 @@
 //   console.log(window.screenX);
 //   console.log(window.screenY);
 // });
+
+/************ DOM: Métodos ****************/
+
+// window.alert("Alerta");
+// window.confirm("Confirmación");
+// window.prompt("Aviso");
+// const $btnAbrir = document.getElementById("abrir-ventana");
+// const $btnCerrar = document.getElementById("cerrar-ventana");
+// const $btnImprimir = document.getElementById("imprimir-ventana");
+
+// let ventana;
+
+// $btnAbrir.addEventListener("click", (e) => {
+//   ventana = window.open("https://jonmircha.com");
+// });
+
+// $btnCerrar.addEventListener("click", (e) => {
+//   // window.close();
+//   ventana.close();
+// });
+
+// $btnImprimir.addEventListener("click", (e) => {
+//   window.print();
+// });
+
+/************ DOM: Objetos: URL, Historial y Navegador ****************/
+
+// console.log("****** Objeto URL (location) ******");
+// console.log(location);
+// console.log(location.origin);
+// console.log(location.protocol);
+// console.log(location.host);
+// console.log(location.hostname);
+// console.log(location.port);
+// console.log(location.href);
+// console.log(location.hash);
+// console.log(location.search);
+// console.log(location.pathname);
+// location.reload();
+
+// console.log("****** Objeto Historial (history) ******");
+// console.log(history);
+// console.log(history.length);
+// console.log(history.back(2));
+// console.log(history.forward(1));
+// console.log(history.go(-1));
+
+// console.log("****** Objeto Navegador (navigator) ******");
+// console.log(navigator);
+// console.log(navigator.connection);
+// console.log(navigator.geolocation);
+// console.log(navigator.mediaDevices);
+// console.log(navigator.mimeTypes);
+// console.log(navigator.onLine);
+// console.log(navigator.serviceWorker);
+// console.log(navigator.storage);
+// console.log(navigator.usb);
+// console.log(navigator.userAgent);
