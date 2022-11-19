ffea57d9fcd26653f688f8017931317fb0366932
Renzo Osorio <renzo.osorio1909@gmail.com>
2022-11-18 20:33:39 -0500

    video 89: Ejercicios Prácticos API localStorage

--- js/index_dom.js
+++ js/index_dom.js
@@ -1,6 +1,10 @@
 import hamburgerMenu from "./dom/menu_hamburguesa.js";
 import { digitalClock, alarm } from "./dom/reloj.js";
 import { shortcuts } from "./dom/teclado.js";
+import { moveBall } from "./dom/teclado.js";
+import countdown from "./dom/cuenta_regresiva.js";
+import scrollTopBottom from "./dom/boton_scroll.js";
+import darkTheme from "./dom/tema_oscuro.js";
 
 const d = document;
 
@@ -8,8 +12,17 @@ d.addEventListener("DOMContentLoaded", (e) => {
   hamburgerMenu(".panel-btn", ".panel", ".menu a");
   digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
   alarm("assets/alarma.mp3", "#activar-alarma", "#desactivar-alarma");
+  countdown(
+    "countdown",
+    "May 23, 2021 03:23:19",
+    "Feliz Cumpleaños amigo y docente digital"
+  );
+  scrollTopBottom(".scroll-top-btn");
 });
 
 d.addEventListener("keydown", (e) => {
   shortcuts(e);
+  moveBall(e, ".ball", ".stage");
 });
+
+darkTheme(".dark-theme-btn", "dark-mode");
